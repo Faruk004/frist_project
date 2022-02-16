45a411543a7e05c3b83140baf69767a6c22efefb
Faruk <farukofficial20@gmail.com>
2022-02-16 13:10:47 +0600

    hello

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,67 @@
 import 'dart:io';
 
 void main() {
+  /////// Ans to the Question No: 1 /////////
   String h = 'Hello';
+  String name = 'Mohibbullah Faruk';
 
-  print(h);
+  print('Start: $h \nMy name is $name');
+
+  /////// Ans to the Question No: 2 /////////
+  int Num1 = 100;
+  int Num2 = 20;
+  int sum = Num1 + Num2;
+
+  print('$Num1+$Num2 = $sum');
+
+  /////// Ans to the Question No: 3 /////////
+  int BatchNo = 12;
+  String Roll = '13';
+  String name1 = 'Mohibbullah Faruk';
+  double Cgpa = 3.5;
+  List userL = [
+    'My name is Mohibbullah Faruk',
+    'My father name is Ali Ashraf',
+    'My sister name is Fatema'
+  ];
+
+  List studentList = ['1', '2', '3', '4'];
+
+  Map map = {'id': '110', 'name': 'Mohibbullah Faruk'};
+
+  print(
+      'Batch No: $BatchNo \nRoll No: $Roll \nMy name is $name1 \ncgpa: $Cgpa \n$userL \n$studentList \n$map');
+
+  /////// Ans to the Question No: 4 /////////
+  int a = 30;
+  int b = 30;
+
+  int Multiplication = a * b;
+
+  print('$a*$b = $Multiplication');
+
+  /////// Ans to the Question No: 5 ///////// sum (addition), multiply, subtract, divide and remainder of two numbers.
+
+  int x = 20;
+  int y = 30;
+
+  int sum1 = x + y;
+  int sub = x - y;
+  int multi = x * y;
+  int divide = x ~/ y;
+
+  print("20+30 = ${x + y}");
+  print("20-30 = ${x - y}");
+  print("20*30 = ${x * y}");
+  print("20/30 = ${x / y}");
+
+  /////// Ans to the Question No: 6 /////////
+
+  /////// Ans to the Question No: 7 /////////
+
+  /////// Ans to the Question No: 8 /////////
+
+  /////// Ans to the Question No: 9 /////////
+
+  /////// Ans to the Question No: 10 /////////
 }
