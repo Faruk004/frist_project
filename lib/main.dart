80b4af831bcb8a43c72ecd4190c25a9fe6563be9
Faruk <farukofficial20@gmail.com>
2022-02-24 19:17:33 +0600

    second

--- lib/main.dart
+++ lib/main.dart
@@ -1,100 +1,123 @@
-import 'dart:io';
+// import 'dart:io';
 
-void main() {
-  /////// Ans to the Question No: 1 /////////
-  String h = 'Hello';
-  String name = 'Mohibbullah Faruk';
+// void main() {
+//   /////// Ans to the Question No: 1 /////////
+//   String h = 'Hello';
+//   String name = 'Mohibbullah Faruk';
 
-  print('Start: $h \nMy name is $name');
+//   print('Start: $h \nMy name is $name');
 
-  /////// Ans to the Question No: 2 /////////
-  int Num1 = 100;
-  int Num2 = 20;
-  int sum = Num1 + Num2;
+//   /////// Ans to the Question No: 2 /////////
+//   int Num1 = 100;
+//   int Num2 = 20;
+//   int sum = Num1 + Num2;
 
-  print('$Num1+$Num2 = $sum');
+//   print('$Num1+$Num2 = $sum');
 
-  /////// Ans to the Question No: 3 /////////
-  int BatchNo = 12;
-  String Roll = '13';
-  String name1 = 'Mohibbullah Faruk';
-  double Cgpa = 3.5;
-  List userL = [
-    'My name is Mohibbullah Faruk',
-    'My father name is Ali Ashraf',
-    'My sister name is Fatema'
-  ];
+//   /////// Ans to the Question No: 3 /////////
+//   int BatchNo = 12;
+//   String Roll = '13';
+//   String name1 = 'Mohibbullah Faruk';
+//   double Cgpa = 3.5;
+//   List userL = [
+//     'My name is Mohibbullah Faruk',
+//     'My father name is Ali Ashraf',
+//     'My sister name is Fatema'
+//   ];
 
-  List studentList = ['1', '2', '3', '4'];
+//   List studentList = ['1', '2', '3', '4'];
 
-  Map map = {'id': '110', 'name': 'Mohibbullah Faruk'};
+//   Map map = {'id': '110', 'name': 'Mohibbullah Faruk'};
 
-  print(
-      'Batch No: $BatchNo \nRoll No: $Roll \nMy name is $name1 \ncgpa: $Cgpa \n$userL \n$studentList \n$map');
+//   print(
+//       'Batch No: $BatchNo \nRoll No: $Roll \nMy name is $name1 \ncgpa: $Cgpa \n$userL \n$studentList \n$map');
 
-  /////// Ans to the Question No: 4 /////////
-  int a = 30;
-  int b = 30;
+//   /////// Ans to the Question No: 4 /////////
+//   int a = 30;
+//   int b = 30;
 
-  int Multiplication = a * b;
+//   int Multiplication = a * b;
 
-  print('$a*$b = $Multiplication');
+//   print('$a*$b = $Multiplication');
 
-  /////// Ans to the Question No: 5 /////////
+//   /////// Ans to the Question No: 5 /////////
 
-  int x = 20;
-  int y = 30;
+//   int x = 20;
+//   int y = 30;
 
-  int sum1 = x + y;
-  int sub = x - y;
-  int multi = x * y;
-  int divide = x ~/ y;
+//   int sum1 = x + y;
+//   int sub = x - y;
+//   int multi = x * y;
+//   int divide = x ~/ y;
 
-  print("20+30 = ${x + y}");
-  print("20-30 = ${x - y}");
-  print("20*30 = ${x * y}");
-  print("20/30 = ${x / y}");
+//   print("20+30 = ${x + y}");
+//   print("20-30 = ${x - y}");
+//   print("20*30 = ${x * y}");
+//   print("20/30 = ${x / y}");
 
-  /////// Ans to the Question No: 6 /////////
+//   /////// Ans to the Question No: 6 /////////
 
-  /////// Ans to the Question No: 7 /////////
-  Map info = {
-    'id': '110',
-    'name': 'Mohibbullah Faruk',
-    'My Gmail': 'farukofficial20@gmail.com',
-    'My Phone Number': '01890-700306'
-  };
+//   /////// Ans to the Question No: 7 /////////
+//   Map info = {
+//     'id': '110',
+//     'name': 'Mohibbullah Faruk',
+//     'My Gmail': 'farukofficial20@gmail.com',
+//     'My Phone Number': '01890-700306'
+//   };
 
-  print(info);
+//   print(info);
 
-  /////// Ans to the Question No: 8 /////////
+//   /////// Ans to the Question No: 8 /////////
 
-  int num5 = 55;
-  int num6 = 9;
+//   int num5 = 55;
+//   int num6 = 9;
 
-  double math = -5 + 8 * 6;
-  double mathb = (55 + 9) % 9;
-  double mathc = 20 + -3 * 5 / 8;
-  double mathd = 5 + 15 / 3 * 2 - 8 % 3;
+//   double math = -5 + 8 * 6;
+//   double mathb = (55 + 9) % 9;
+//   double mathc = 20 + -3 * 5 / 8;
+//   double mathd = 5 + 15 / 3 * 2 - 8 % 3;
 
-  print(math);
-  print((num5 + num6) % num6);
-  print(mathc);
-  print(mathd);
+//   print(math);
+//   print((num5 + num6) % num6);
+//   print(mathc);
+//   print(mathd);
 
-  /////// Ans to the Question No: 9 /////////
-  String ans9 = ('Data-type: One of the most fundamental characteristics of a programming language is the set of data types it supports.'
-      'These are the type of values that can be represented and manipulated in a programming language.'
-      'The Dart language supports the following types−'
-      'Numbers'
-      'Strings'
-      'Booleans'
-      'Lists'
-      'Maps'
-      'Case-sensitive: Dart is case-sensitive. This means that Dart differentiates between uppercase and lowercase characters.'
-      'equal operator: Assignment operators are used to assign value to a variable');
+//   /////// Ans to the Question No: 9 /////////
+//   String ans9 = ('Data-type: One of the most fundamental characteristics of a programming language is the set of data types it supports.'
+//       'These are the type of values that can be represented and manipulated in a programming language.'
+//       'The Dart language supports the following types−'
+//       'Numbers'
+//       'Strings'
+//       'Booleans'
+//       'Lists'
+//       'Maps'
+//       'Case-sensitive: Dart is case-sensitive. This means that Dart differentiates between uppercase and lowercase characters.'
+//       'equal operator: Assignment operators are used to assign value to a variable');
 
-  /////// Ans to the Question No: 10 /////////
-  String ans =
-      ('Variables is an identifier used to refer memory location in computer memory that holds a value for that variable');
-}
+//   /////// Ans to the Question No: 10 /////////
+//   String ans =
+//       ('Variables is an identifier used to refer memory location in computer memory that holds a value for that variable');
+// }
+
+// void main() {
+//   int sum = add(10, 20);
+//   print(sum);
+// }
+
+// int add(
+//   int n1,
+//   int n2,
+// ) {
+//   int sum = n1 + n2;
+//   return sum;
+// }
+
+// import 'dart:js_util';
+
+// void main() {
+//   var odd_list = [1, 3, 5, 7, 9];
+//   print(odd_list);
+//   // odd_list.insertAll(1, [12, 13, 14]);
+//   odd_list[2] = 55;
+//   print(odd_list);
+// }
