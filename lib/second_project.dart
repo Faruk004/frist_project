b30de5236ceec10da29107cdf630c9a176012d7a
Faruk <farukofficial20@gmail.com>
2022-02-24 20:32:22 +0600

    \

--- lib/second_project.dart
+++ lib/second_project.dart
@@ -1,5 +1,64 @@
 void main() {
-  var List = ['Hello'];
+  ///==== Ans to the Question No: 01 ======///
 
-  print(List);
+  List student_list = [
+    '1.Roni \n'
+        '2.Jony \n'
+        '3.Rakib \n'
+        '4.Shakib \n'
+        '5.Tamim \n'
+  ];
+  print(student_list);
+
+  ///==== Ans to the Question No: 02 ======///
+
+  List student_list2 = [
+    '1.Roni \n'
+        '2.Jony \n'
+        '3.Rakib \n'
+        '4.Shakib \n'
+        '5.Tamim \n'
+  ];
+  print(student_list2);
+
+  ///==== Ans to the Question No: 03 ======///
+
+  List student_list3 = [
+    [1, 2, 3],
+    {'id': 110, 'name': 'Faruk'},
+    35.2,
+    [],
+    {},
+    1,
+    'Hello flutter',
+  ];
+  print(student_list3);
+
+  ///==== Ans to the Question No: 04 ======///
+
+  ///==== Ans to the Question No: 05 ======///
+
+  // var a = double.parse("Hello");
+  // var list = [];
+
+  // try {
+  //   print(list[0]);
+  // } catch (e, l) {
+  //   print('Error type: $e');
+  //   print('Line: $l');
+  // } finally {
+  //   print('finally bloc');
+  // }
+
+  // int b = int.parse("10.55");
+
+  ///==== Ans to the Question No: 06 ======///
+
+  ///==== Ans to the Question No: 07 ======///
+
+  ///==== Ans to the Question No: 08 ======///
+
+  ///==== Ans to the Question No: 09 ======///
+
+  ///==== Ans to the Question No: 10 ======///
 }
